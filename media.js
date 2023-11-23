c27e07a55ddb7f435456ab1f9d7dc3ecf354ee0c
Marcos Lira <53446701+Marcoslira91@users.noreply.github.com>
2023-11-23 15:39:14 -0300

    Create media.js
    Faça um programa em JavaScript que calcule a média de três notas inseridas pelo usuário usando a função prompt(). Ela deve ser armazenada em variáveis adequadas, por exemplo: nota1, nota2 e nota3, com o tipo de dado Number. Depois, some-as e divida por três. O resultado deste cálculo será o valor da média , que deve ser armazenado em uma variável chamada media. Após obter a media, deverar ser exibido esse valor no console usando a função console.log().

---
+++ media.js
@@ -0,0 +1,10 @@
+// Solicita ao usuário que insira as notas
+var nota1 = parseFloat(prompt("Insira a primeira nota:"));
+var nota2 = parseFloat(prompt("Insira a segunda nota:"));
+var nota3 = parseFloat(prompt("Insira a terceira nota:"));
+
+// Calcula a média das notas
+var media = (nota1 + nota2 + nota3) / 3;
+
+// Exibe a média no console
+console.log("A média das notas é: " + media);
